177843e6889dbb63d0d001d4b3d1dafd1ccc938b
Urinrin Ogidiama <urinrinogidiama@gmail.com>
2026-03-01 20:05:46 +0000

    CORS policy updated

--- src/app.ts
+++ src/app.ts
@@ -4,17 +4,27 @@ import trackerRoutes from "./modules/tracker/tracker.routes";
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173",
+  process.env.CLIENT_URL,
+  process.env.CLIENT_URL_WWW,
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   }),
 );
 
 app.use(express.json());
-
 app.get("/api/health", (_, res) => res.json({ ok: true }));
-
 app.use("/api/tracker", trackerRoutes);
 
 export default app;
